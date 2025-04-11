b33439fad27b304a0f263ce015bd94101d45ccba
nikashitsa <vernik91@gmail.com>
2025-04-11 14:10:39 +0200

    feat: init

---
+++ WidgetExtension/WidgetExtensionBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetExtensionBundle.swift
+//  WidgetExtension
+//
+//  Created by Nikita Verkhovin on 11.04.2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetExtension()
+    }
+}
