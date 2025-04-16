656fd5e8d6ea6b31c6ddba9828705039e63a33c0
nikashitsa <vernik91@gmail.com>
2025-04-16 13:59:57 +0200

    wip

--- WidgetExtension/WidgetExtension.swift
+++ WidgetExtension/WidgetExtension.swift
@@ -7,28 +7,56 @@
 
 import WidgetKit
 import SwiftUI
+import SDWebImage
+import SDWebImageSVGNativeCoder
 
 struct Provider: AppIntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
+        SimpleEntry(date: Date(), image: nil, configuration: ConfigurationAppIntent())
     }
 
     func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: configuration)
+        SimpleEntry(date: Date(), image: nil, configuration: configuration)
     }
     
     func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
-        var entries: [SimpleEntry] = []
+//        var entries: [SimpleEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
+//        let currentDate = Date()
+//        for hourOffset in 0 ..< 5 {
+//            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+//            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+//            entries.append(entry)
+//        }
+        
+        
+        let image = await downloadImage(url: "https://price.coin.space/logo/dogecoin.svg?ver=211")
+        let entry = SimpleEntry(date: Date(), image: image, configuration: configuration)
+        
+        print("timeline")
 
-        return Timeline(entries: entries, policy: .atEnd)
+        return Timeline(entries: [entry], policy: .atEnd)
+    }
+    
+    private func downloadImage(url: String) async -> UIImage? {
+        return await withCheckedContinuation { continuation in
+            SDImageCodersManager.shared.addCoder(SDImageSVGNativeCoder.shared)
+            let thumbnailSize = CGSize(width: 128, height: 128)
+            
+            SDWebImageManager.shared.loadImage(
+                with: URL(string: url),
+                options: [.retryFailed],
+                context: [.imageThumbnailPixelSize: thumbnailSize],
+                progress: nil) { image, _, error, _, _, _ in
+                    if let img = image {
+                        continuation.resume(returning: img)
+                    } else {
+                        print("Image download error: \(String(describing: error))")
+                        continuation.resume(returning: nil)
+                    }
+            }
+        }
     }
 
 //    func relevances() async -> WidgetRelevances<ConfigurationAppIntent> {
@@ -38,6 +66,7 @@ struct Provider: AppIntentTimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
+    let image: UIImage?
     let configuration: ConfigurationAppIntent
 }
 
@@ -46,12 +75,43 @@ struct WidgetExtensionEntryView : View {
 
     var body: some View {
         VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Favorite Emoji:")
-            Text(entry.configuration.favoriteEmoji)
+            HStack(alignment: .top) {
+                if let uiImage = entry.image {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .frame(width: 32.0, height: 32.0)
+                } else {
+                    Circle()
+                        .fill(.orange)
+                        .frame(width: 32.0, height: 32.0)
+                }
+                Spacer()
+                Text("+3.43%")
+                    .setFontStyle(WidgetFonts.textXs)
+                    .foregroundColor(WidgetColors.primary)
+            }
+            .frame(
+                  maxWidth: .infinity,
+                  alignment: .topLeading
+            )
+            Spacer()
+            VStack(alignment: .leading, spacing: 0.0) {
+                Text("Bitcoin")
+                    .setFontStyle(WidgetFonts.textMd)
+                    .foregroundColor(WidgetColors.secondary)
+                Text("$38,638.36")
+                    .setFontStyle(WidgetFonts.textMdBold)
+                    .foregroundColor(WidgetColors.textColor)
+            }
+            .frame(
+                maxWidth: .infinity,
+                alignment: .topLeading
+            )
         }
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: .infinity
+        )
     }
 }
 
@@ -61,7 +121,7 @@ struct WidgetExtension: Widget {
     var body: some WidgetConfiguration {
         AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
             WidgetExtensionEntryView(entry: entry)
-                .containerBackground(.fill.tertiary, for: .widget)
+                .containerBackground(.white, for: .widget)
         }
     }
 }
@@ -83,6 +143,5 @@ extension ConfigurationAppIntent {
 #Preview(as: .systemSmall) {
     WidgetExtension()
 } timeline: {
-    SimpleEntry(date: .now, configuration: .smiley)
-    SimpleEntry(date: .now, configuration: .starEyes)
+    SimpleEntry(date: .now, image: nil, configuration: .smiley)
 }
---
+++ WidgetExtension/WidgetVariables.swift
@@ -0,0 +1,32 @@
+import SwiftUICore
+
+struct FontStyle {
+    let size: CGFloat
+    let weight: Font.Weight
+    let lineHeight: CGFloat
+    let letterSpacing: CGFloat
+}
+
+struct WidgetFonts {
+    static let textXs = FontStyle(size: 12, weight: .regular, lineHeight: 1.5, letterSpacing: 0.03)
+    static let textXsBold = FontStyle(size: 12, weight: .semibold, lineHeight: 1.5, letterSpacing: 0.03)
+    
+    static let textMd = FontStyle(size: 18, weight: .regular, lineHeight: 1.5, letterSpacing: 0.01)
+    static let textMdBold = FontStyle(size: 18, weight: .semibold, lineHeight: 1.5, letterSpacing: 0.01)
+}
+
+extension Text {
+    func setFontStyle(_ fontStyle: FontStyle) -> some View {
+        self.font(Font.system(size: fontStyle.size, weight: fontStyle.weight))
+            .kerning(fontStyle.size * fontStyle.letterSpacing)
+            .frame(height: fontStyle.size * fontStyle.lineHeight)
+            .lineLimit(1)
+    }
+}
+
+struct WidgetColors {
+    static let primary = Color("Primary")
+    static let secondary = Color("Secondary")
+    static let danger = Color("Danger")
+    static let textColor = Color("TextColor")
+}
